1f3033627010ce37e69da13ee1c0add68a2833d9
SDubaj <dubajsebastian96@gmail.com>
2020-03-13 12:32:24 +0100

    Updates

---
+++ precache-manifest.5aeb6a9e41e7f9065dadfb4b7fb036a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a6fc96454b01f3d9ae8d6847d3ca3e",
+    "url": "/RandomMeal/index.html"
+  },
+  {
+    "revision": "3d14bcab803c9c75b7e0",
+    "url": "/RandomMeal/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1139e3d71e2a837f3d6b",
+    "url": "/RandomMeal/static/css/main.6b8f1a95.chunk.css"
+  },
+  {
+    "revision": "3d14bcab803c9c75b7e0",
+    "url": "/RandomMeal/static/js/2.aa1421f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RandomMeal/static/js/2.aa1421f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1139e3d71e2a837f3d6b",
+    "url": "/RandomMeal/static/js/main.7ff3c408.chunk.js"
+  },
+  {
+    "revision": "dc8d77e213599213cb5e",
+    "url": "/RandomMeal/static/js/runtime-main.6572db35.js"
+  }
+]);
